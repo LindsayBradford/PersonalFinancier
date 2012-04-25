1f28fa3ffd0cbb10eb8f8bafd089591cf6ac588e
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2012-04-25 23:07:07 +1000

    GUI Base tabbing, message bar, plus look & feel set.

--- src/blacksmyth/personalfinancier/PersonalFinancier.java
+++ src/blacksmyth/personalfinancier/PersonalFinancier.java
@@ -7,8 +7,19 @@
 
 package blacksmyth.personalfinancier;
 
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
+
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 
 public class PersonalFinancier {
 
@@ -20,7 +31,7 @@ public class PersonalFinancier {
   public static void main(String[] args) {
     try {
       UIManager.setLookAndFeel(
-          "javax.swing.plaf.metal.MetalLookAndFeel"
+          "com.jtattoo.plaf.hifi.HiFiLookAndFeel"
         );
     } catch (Exception e) {
       e.printStackTrace();
@@ -33,6 +44,64 @@ public class PersonalFinancier {
 class PersonalFinancierFactory {
   
   public static JFrame createJFrame() {
-    return new JFrame("Personal Financier");
+    
+    JFrame frame = new JFrame("Personal Financier");
+    
+    frame.getContentPane().add(
+        createContentPane(), 
+        BorderLayout.CENTER
+    );
+    
+    frame.getContentPane().add(
+      createMessageBar(),
+      BorderLayout.PAGE_END
+    );
+   
+    frame.setBounds(
+        0, 0,    // x,y coords
+        Toolkit.getDefaultToolkit().getScreenSize().width, 
+        Toolkit.getDefaultToolkit().getScreenSize().height
+    );
+
+    
+    return frame;
+  }
+  
+  
+  private static JComponent createContentPane() {
+    JTabbedPane pane = new JTabbedPane();
+    pane.setBorder(new EmptyBorder(5,5,5,5));
+    
+    pane.addTab("Budget", createBudgetComponent());
+    pane.addTab("Ledgers", createLedgersComponent());
+    
+    return pane;
+  }
+  
+  private static JComponent createBudgetComponent() {
+    return new JLabel("Budget Component");
+  }
+  
+  private static JComponent createLedgersComponent() {
+    return new JLabel("Ledgers Component");
+  }
+  
+  private static JComponent createMessageBar() {
+    
+    JPanel messagePanel = new JPanel(new BorderLayout());
+    messagePanel.setBorder(new EmptyBorder(2,5,5,5));
+    
+    JLabel messageLabel = new JLabel("test");
+   
+    messageLabel.setBorder(
+       new CompoundBorder(
+           new BevelBorder(BevelBorder.LOWERED),
+           new EmptyBorder(2,5,5,5)
+       )
+    );
+   
+    messagePanel.add(messageLabel, BorderLayout.CENTER);
+  
+    return messagePanel;
   }
 }
