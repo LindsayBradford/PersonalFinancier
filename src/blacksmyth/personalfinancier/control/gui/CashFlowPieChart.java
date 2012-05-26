93eb2e49041df772095580da048c4da3096bc924
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2012-05-26 22:03:53 +1000

    CashFlowPieChart reimplemented with GRAL.

--- src/blacksmyth/personalfinancier/control/gui/CashFlowPieChart.java
+++ src/blacksmyth/personalfinancier/control/gui/CashFlowPieChart.java
@@ -1,179 +1,210 @@
 package blacksmyth.personalfinancier.control.gui;
 
-import java.awt.Dimension;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.LinearGradientPaint;
+import java.awt.Shape;
+import java.math.BigDecimal;
 import java.util.Observable;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
-import com.googlecode.charts4j.GCharts;
-import com.googlecode.charts4j.PieChart;
-import com.googlecode.charts4j.Slice;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import blacksmyth.personalfinancier.control.IBudgetObserver;
+import blacksmyth.personalfinancier.model.BigDecimalFactory;
 import blacksmyth.personalfinancier.model.budget.AccountSummary;
 import blacksmyth.personalfinancier.model.budget.BudgetModel;
 
-public class CashFlowPieChart extends JLabel implements IBudgetObserver, Runnable {
-  private BudgetModel budgetModel;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.graphics.Drawable;
+import de.erichseifert.gral.plots.PiePlot;
+import de.erichseifert.gral.plots.PiePlot.PieSliceRenderer;
+import de.erichseifert.gral.plots.PlotArea;
+import de.erichseifert.gral.plots.points.PointData;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.Insets2D;
+import de.erichseifert.gral.util.Location;
+
+public class CashFlowPieChart extends JPanel implements IBudgetObserver {
+  
+  private static final int IS_NEGATIVE_COLUMN = 1;
+  private static final int LABEL_COLUMN = 2;
+  
+  BudgetModel model;
   
   public CashFlowPieChart(BudgetModel model) {
-    this.budgetModel = model;
+    super(new BorderLayout());
+    
+    this.setBorder(new EmptyBorder(5,5,5,5));
+    this.setBackground(Color.WHITE);
+    
+    setModel(model);
+  }
+  
+  private void setModel(BudgetModel model) {
+    this.model = model;
     model.addObserver(this);
-    
-    this.setVerticalAlignment(CENTER);
-    this.setHorizontalAlignment(CENTER);
-
-    final CashFlowPieChart chart = this;
-    
-    this.addComponentListener(new ComponentAdapter() {
-      public void componentResized(ComponentEvent e) {
-        RunnableQueueThread.getInstance().push(chart);
-      }
-    });
-  }
-
-  public void update(Observable observable, Object arg1) {
-    RunnableQueueThread.getInstance().push(this);
-  }
-  
-  public void run() {
-    refreshIcon();
-  }
-  
-  private void refreshIcon() {
-    
-    try {
-      ImageIcon graphIcon = new ImageIcon(
-          ImageIO.read(
-              buildGraphURL(budgetModel)
-          )
-      );
-      this.setIcon(graphIcon);
-    } catch (IOException e) {
-      e.printStackTrace();
-      this.setIcon(null);
-    }
-    
-  }
-  
-  private URL buildGraphURL(BudgetModel model) {
-    try {
-      return new URL(
-          buildGraphURLString(model)
-      );
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-  
-  private String buildGraphURLString(BudgetModel model) {
-    ArrayList<Slice> slices = new ArrayList<Slice>();
-    
-    int blackOpacity = 100;
-    int redOpacity = 100;
-    
-    com.googlecode.charts4j.Color blackColor = com.googlecode.charts4j.Color.BLACK;
-    com.googlecode.charts4j.Color redColor = com.googlecode.charts4j.Color.RED;
-    
-    double netCashFlow = 0;
-    double total = 0;
-    double incomeTotal = 0;
-    
-    double minValue = 0;
-    double maxValue = 0;
-    
-    for (AccountSummary account : model.getCashFlowSummaries()) {
-      
-      double summaryFigure = account.getBudgettedAmount().getTotal().doubleValue();
-      
-      if (summaryFigure < minValue) {
-        minValue = summaryFigure;
-      }
-      
-      if (summaryFigure > maxValue) {
-        maxValue = summaryFigure;
-      }
-      
-      netCashFlow = netCashFlow + summaryFigure;
-      
-      double absSummaryFigure = Math.abs(summaryFigure);
-      
-      total = total + absSummaryFigure;
-      if (summaryFigure > 0) {
-        incomeTotal = incomeTotal + summaryFigure;
-      }
-    }
-    
-    for (AccountSummary account : model.getCashFlowSummaries()) {
-      
-      double summaryFigure = account.getBudgettedAmount().getTotal().doubleValue();
-      
-      double absSummaryFigure = Math.abs(summaryFigure);
-      int percentageAsInteger = (int) Math.round(((absSummaryFigure / total) * 100));
-      
-      com.googlecode.charts4j.Color sliceColor = summaryFigure < 0 ? redColor : blackColor;
-
-      sliceColor = com.googlecode.charts4j.Color.newColor(
-          sliceColor, 
-          summaryFigure < 0 ? redOpacity : blackOpacity
-      );
-
-      Slice accountSlice = Slice.newSlice(
-          percentageAsInteger, 
-          sliceColor,
-          account.getAccountNickname() + ", " + WidgetFactory.DECIMAL_FORMAT.format(summaryFigure)
-      );
-
-      if (summaryFigure < 0) {
-        redOpacity -= 20;
-      } else {
-        blackOpacity -= 20;
-      }
-
-      slices.add(accountSlice);
-    }
-    
-    String surplusOrDeficit = netCashFlow >= 0 ? "Surplus" : "Deficit";
-    com.googlecode.charts4j.Color surplusOrDeficitColor = netCashFlow >= 0 ? com.googlecode.charts4j.Color.BLACK : com.googlecode.charts4j.Color.RED;
-    
-    PieChart chart = GCharts.newPieChart(slices);
-    chart.setTitle(
-        "Net Cash Flow " + surplusOrDeficit + ": " + WidgetFactory.DECIMAL_FORMAT.format(netCashFlow), 
-        surplusOrDeficitColor, 
-        16
-    );
-    
-    chart.setTransparency(85);
-
-    try {
-      chart.setSize(
-          this.getPreferredSize().width,
-          this.getPreferredSize().height
-      );
-    } catch (Exception e) {
-      chart.setSize(500,200);
-    }
-    
-    return chart.toURLString();
   }
   
   @Override
-  public Dimension getPreferredSize() {
-    Dimension theSize = this.getParent().getSize();
-    theSize.width = Math.max(theSize.width - 5, 500);
-    theSize.height = Math.max(theSize.height - 5, 200);
-    
-    return theSize;
+  public void update(Observable arg0, Object arg1) {
+    refreshSelf();
   }
+  
+  @SuppressWarnings("unchecked")
+  private void refreshSelf() {
 
+    DataTable accountCashFlows = new DataTable(Double.class, Boolean.class, String.class);
+    
+    BigDecimal netCashFlow = BigDecimalFactory.create(0);
 
+    for(AccountSummary summary : model.getCashFlowSummaries()) {
+      
+      double cashFlow = summary.getBudgettedAmount().getTotal().doubleValue();
+      
+      accountCashFlows.add(
+          Math.abs(cashFlow),  // negative values in PiePlot render as no slice
+          cashFlow >= 0 ? false : true,
+          summary.getAccountNickname() + ", " + 
+          WidgetFactory.DECIMAL_FORMAT.format(
+              cashFlow
+          )
+      );
+      
+      netCashFlow = netCashFlow.add(
+          summary.getBudgettedAmount().getTotal()
+      );
+    }
+    
+    displayPiePlot(
+        createPiePlot(
+            accountCashFlows,
+            getTitleText(netCashFlow)
+         )
+     );
+  }
+  
+  private String getTitleText(BigDecimal netCashFlow) {
+    String surplusOrDeficit = netCashFlow.doubleValue() >= 0 ? "Surplus" : "Deficit";
+    return "Net Cash Flow " + surplusOrDeficit + ": " + WidgetFactory.DECIMAL_FORMAT.format(netCashFlow.doubleValue());
+  }
+  
+  private void displayPiePlot(PiePlot plot) {
+    this.removeAll();  // destroy old PiePlot
+
+    this.add(
+        new InteractivePanel(plot), 
+        BorderLayout.CENTER
+    );
+    
+    this.validate();  // need to force a redraw
+  }
+  
+  private PiePlot createPiePlot(DataTable cashFlowData, String title) {
+    PiePlot plot = new PiePlot(cashFlowData);
+    
+    plot.setSetting(PiePlot.TITLE, title);
+    
+    plot.getPlotArea().setSetting(PlotArea.BORDER,null);
+
+    plot.setInsets(new Insets2D.Double(1.0, 1.0, 1.0, 1.0));
+    
+    PieSliceRenderer positivePointRenderer = new PiePlot.PieSliceRenderer(plot);
+    
+    configurePieSliceRenderer(positivePointRenderer, Color.BLACK);
+    
+    PieSliceRenderer negativePointRenderer = new PiePlot.PieSliceRenderer(plot);
+
+    configurePieSliceRenderer(negativePointRenderer, Color.RED);
+
+    plot.setPointRenderer(
+        cashFlowData, 
+        new DualSliceRenderer(
+            plot,
+            positivePointRenderer,
+            negativePointRenderer
+        )
+    );
+    
+    return plot;
+  }
+  
+  /**
+   * Utility function that ensures a number of related PieSliceRenderer
+   * instances are configured to differ only in rendering a single colour.
+   * @param renderer
+   * @param color
+   */
+  private void configurePieSliceRenderer(PieSliceRenderer renderer, Color color) {
+    renderer.setSetting(PieSliceRenderer.GAP, 0.1);
+
+    renderer.setSetting(
+        PointRenderer.COLOR,
+        new LinearGradientPaint(
+            0f,0f, 0f,1f,
+            new float[] { 0.0f, 1.0f },
+            new Color[] { color, Color.GRAY }
+        )
+    );
+
+    renderer.setSetting(PointRenderer.VALUE_DISPLAYED, true);
+    renderer.setSetting(PointRenderer.VALUE_LOCATION, Location.NORTH);
+    
+    renderer.setSetting(PointRenderer.VALUE_COLUMN, LABEL_COLUMN);
+    renderer.setSetting(PointRenderer.VALUE_COLOR, color);
+  }
+  
+  /**
+   * A PieSliceRenderer that delegates rendering to a positive or a
+   * negative PieSliceRenderer based on whether the data point to 
+   * render is a positive or negative number. 
+   * @author linds
+   *
+   */
+  private class DualSliceRenderer extends PieSliceRenderer {
+    
+    private PiePlot plot;
+
+    private PieSliceRenderer positiveSliceRenderer;
+    private PieSliceRenderer negativeSliceRenderer;
+
+    public DualSliceRenderer(PiePlot plot, PieSliceRenderer positiveSliceRenderer, 
+                                           PieSliceRenderer negativeSliceRenderer) {
+      super(plot);
+      this.plot = plot;
+      this.positiveSliceRenderer = positiveSliceRenderer;
+      this.negativeSliceRenderer = negativeSliceRenderer;
+    }
+
+    @Override
+    public Drawable getPoint(PointData data, Shape shape) {
+      if (valueIsNegative(data)) {
+        return negativeSliceRenderer.getPoint(data, shape);
+      }
+      return positiveSliceRenderer.getPoint(data, shape);
+    }
+    
+    @Override 
+    public Shape getPointShape(PointData data) {
+      if (valueIsNegative(data)) {
+        return negativeSliceRenderer.getPointShape(data);
+      }
+      return positiveSliceRenderer.getPointShape(data);
+    }
+    
+    /**
+     * Interrogates the current plot's data value
+     * for the given <tt>data</tt> entry.  If the 
+     * value is negative, returns <tt>true</tt>.
+     * @param data
+     * @return
+     */
+    private boolean valueIsNegative(PointData data) {
+      return (Boolean) plot.getData().get(0).get(
+          IS_NEGATIVE_COLUMN, 
+          data.row.getIndex()
+      );
+    }
+  }
 }
