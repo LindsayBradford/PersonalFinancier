df715c58ab3bda38ede849b084456af568e9635c
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2014-04-13 18:26:47 +1000

    Fixing gradle build script to ship bouncycastle library separately.

--- src/blacksmyth/personalfinancier/control/StrategicFileAdapter.java
+++ src/blacksmyth/personalfinancier/control/StrategicFileAdapter.java
@@ -1,12 +1,39 @@
+/**
+ * Copyright (c) 2012, Lindsay Bradford and other Contributors.
+ * All rights reserved.
+ * 
+ * This program and the accompanying materials  are made available 
+ * under the terms of the BSD 3-Clause licence  which accompanies 
+ * this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ */
+
 package blacksmyth.personalfinancier.control;
 
 import java.util.HashMap;
 
 import blacksmyth.general.file.IObjectFileConverter;
 
-public class StrategicFileAdapter<T> implements IObjectFileConverter<T> {
+/**
+ * This is a concrete implementation of IObjectFileConverter<T> that
+ * interrogates the file extension  of the file specified, and uses that
+ * extension to decide which of the adapter's added to its internal  map 
+ * to delegate operations to.
+ * @param <T>
+ */
+public final class StrategicFileAdapter<T> implements IObjectFileConverter<T> {
   
   private HashMap<String, IObjectFileConverter<T>> adapterMap = new HashMap<String, IObjectFileConverter<T>>();
+
+  /**
+   * Adds the supplied adapter to it's internal map of adapters against which file extensions
+   * they process.
+   * @param fileExtension
+   * @param adapter
+   */
+  public void add(String fileExtension, IObjectFileConverter<T> adapter) {
+    adapterMap.put(fileExtension, adapter);
+  }
 
   @Override
   public T toObjectFromFile(String filePath) {
@@ -24,9 +51,13 @@ public class StrategicFileAdapter<T> implements IObjectFileConverter<T> {
       adapter.toFileFromObject(filePath, t);
     }
   }
-  
-  public void add(String fileExtension, IObjectFileConverter<T> adapter) {
-    adapterMap.put(fileExtension, adapter);
+
+  private IObjectFileConverter<T> getMatchingAdapter(String filePath) {
+    String fileExt = getFileExtension(filePath);
+    
+    IObjectFileConverter<T> adapter = adapterMap.get(fileExt);
+    
+    return adapter;
   }
   
   private String getFileExtension(String filePath) {
@@ -36,13 +67,4 @@ public class StrategicFileAdapter<T> implements IObjectFileConverter<T> {
     }
     return null;
   }
-  
-  private IObjectFileConverter<T> getMatchingAdapter(String filePath) {
-    String fileExt = getFileExtension(filePath);
-    
-    IObjectFileConverter<T> adapter = adapterMap.get(fileExt);
-    
-    return adapter;
-  }
-
 }
